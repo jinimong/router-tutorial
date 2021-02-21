ff0ade120d9a062eaae743dfee8a6d601caabb76
jwkim <phoongdeng@gmail.com>
2021-02-21 23:34:52 +0900

    Add useReactRouter Hook

--- src/Profiles.js
+++ src/Profiles.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
 import WithRouterSample from './WithRouterSample';
+import RouterHookSample from './RouterHookSample';
 
 const Profiles = () => {
   return (
@@ -39,6 +40,7 @@ const Profiles = () => {
       />
       <Route path="/profiles/:username" component={Profile} />
       <WithRouterSample />
+      <RouterHookSample />
     </div>
   );
 };
